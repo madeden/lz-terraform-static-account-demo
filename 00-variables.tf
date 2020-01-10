989abe00ad696c86de878c00fee69866405a5951
Víctor Enríquez <victor.enriquez@sngular.com>
2020-01-10 11:38:56 +0100

    Create some variables

--- 00-variables.tf
+++ 00-variables.tf
@@ -1,3 +1,4 @@
+# Main region
 variable "aws_profile" {
   description = "The AWS profile to use"
   default     = "REPLACE-ME"
@@ -8,27 +9,42 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
+# State bucket goes in the main region
 variable "terraform_state_bucket" {
   description = "ID of the storing-state bucket"
   default     = "REPLACE-ME"
 }
 
+# Replica region
+variable "replica_aws_profile" {
+  description = "The AWS profile to use for replication"
+  default     = "REPLACE-ME"
+}
+
+variable "aws_region_replica" {
+  description = "The AWS region for DR"
+  default     = "us-west-1"
+}
+
+variable "terraform_state_bucket" {
+  description = "ID of the storing-state bucket"
+  default     = "REPLACE-ME"
+}
+
+# The replica name of the bucket will be this one plus -replica
 variable "terraform_website_bucket" {
   description = "ID of the bucket storing the website static files"
   default     = "REPLACE-ME"
 }
 
 provider "aws" {
+  alias   = "main"
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "${var.terraform_state_bucket}"
-    key     = "tfstate/terraform.tfstate"
-    region  = "${var.aws_region}"
-    profile = "${var.aws_profile}"
-  }
+provider "aws" {
+  alias   = "replica"
+  region  = "${var.aws_region_replica}"
+  profile = "${var.replica_aws_profile}"
 }
-
--- 01-tf-s3.tf
+++ 01-tf-s3.tf
@@ -1,6 +1,8 @@
+# State bucket is stored in the main region
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "${var.terraform_state_bucket}"
   acl    = "private"
+  region = "${var.aws_region}"
 
   versioning {
     enabled = true
